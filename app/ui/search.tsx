9a05d64b35ca4a73c19c12c7135a316773b60bbe
张金富 <zjf_sdnu@163.com>
2026-06-29 16:56:05 +0800

    第十章

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,22 +2,24 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
 
-  function handleSearch(term: string) {
-    console.log("Search", term);
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching for ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
